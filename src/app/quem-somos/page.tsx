4b295b4fbf8cb9437fc032871b17246fc0b2873a
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-24 21:49:34 -0300

    feat: add componente quem-somos da home page

---
+++ src/app/quem-somos/page.tsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.css'
+
+export default function QuemSomos(){
+  return(
+    <div className='flex justify-center'>
+      <div className={styles.container}>
+          <p className={styles.text_title}>Quem somos</p>
+          <p className={styles.text}>Gingerbread cookie gingerbread jujubes cake biscuit tart fruitcake. Jujubes chupa chups wafer cheesecake lemon drops. Pastry caramels cookie marshmallow. Chupa chups jelly beans apple pie powder marzipan oat cake jelly-o chupa chups biscuit. Lollipop biscuit jujubes tootsie roll oat cake chocolate bar.</p>
+      </div>
+    </div>
+  )
+}
