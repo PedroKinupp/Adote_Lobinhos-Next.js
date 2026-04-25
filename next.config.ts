28b409780eda4fcceba05f6c93690068b0ce6ee4
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-25 14:52:28 -0300

    config: configurando a tag Image do next js pra usar url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("http://dummyimage.com/**"),
+      new URL("https://dummyimage.com/**"),
+    ],
+  },
 };
 
 export default nextConfig;
