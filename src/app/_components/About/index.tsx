eef4e50d00adc832a7ab19e26a85ee4749d27619
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-24 21:51:18 -0300

    feat: add componente sobre da home page

---
+++ src/app/_components/About/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css'
+
+export default function About() {
+  return (
+    <section className={styles.sobre}>
+        <div className="flex flex-col max-w-225 mx-auto text-center gap-4">
+            <h1 className={styles.title}>Sobre</h1>
+            <p className={styles.text}>
+                Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas. Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.Não obstante, o surgimento do comércio virtual faz parte de um processo de gerenciamento do levantamento das variáveis envolvidas.
+            </p>
+        </div>
+    </section>
+  );
+}
