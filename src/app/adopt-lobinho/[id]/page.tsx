1587b97fa80f10245e7e57afeafaf80a503f35e8
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-26 20:19:31 -0300

    refactor: página de adotar lobihno individual

--- src/app/adopt-lobinho/[id]/page.tsx
+++ src/app/adopt-lobinho/[id]/page.tsx
@@ -1,39 +1,31 @@
+import loboType from '@/types/loboType';
+import Form from './_components'
 import styles from './styles.module.css'
+import Image from 'next/image';
 
-export default async function AdoptLobinho( {
-    params
-}: {
-    params : Promise<{ id : string }>
-} ){
-
+export default async function AdoptLobinho({ params } : { params : Promise<{ id : string }> }){
     const { id } = await params
-  return(
-    <div className={styles.container}>
-        <div className={styles.header}>
-            <div className={styles.frame}>
+    const data = await fetch(`http://localhost:3001/lobinhos?id=${id}`);
+    const [lobo]: loboType[] = await data.json();
+
+    return(
+        <div className={styles.container}>
+            <div className={styles.header}>
+                    <div className={styles.decorativeFrame}>
+                        <Image
+                            src={lobo.imagem}
+                            alt="foto do lobo"
+                            width={150}
+                            height={150}
+                            className={styles.wolfImage}
+                        />
+                    </div> 
+                <div>
+                    <h1 className={styles.title}>Adote o(a) {lobo.nome}</h1>
+                    <h4 className={styles.id}>ID: {id}</h4>
+                </div>
             </div>
-            <div>
-                <h1 className={styles.title}>Adote o(a) NomedoLobo</h1>
-                <h4 className={styles.id}>ID: {id}</h4>
-            </div>
+            <Form/>
         </div>
-        <form className='flex flex-col items-center gap-20'>
-            <div className={styles.name_age_box}>
-                <div className={styles.name_text_box}>
-                    <label className={styles.label}>Seu nome:</label>
-                    <input type="text" className={styles.input} required/>
-                </div>
-                <div className={styles.age_text_box}>
-                    <label className={styles.label}>Idade:</label>
-                    <input type="number" className={styles.input} required/>
-                </div>
-            </div>
-            <div className={styles.email_text_box}>
-                <label className={styles.label}>E-mail:</label>
-                <input type="text" className={styles.input} required/>
-            </div>
-            <button className={styles.button} type="submit">Adotar</button>
-        </form>
-    </div>
-  )
+    )
 }
