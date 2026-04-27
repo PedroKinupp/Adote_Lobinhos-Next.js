0e2fd9521351667098f18dc0b8fb1e3b79cc735a
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-26 23:26:43 -0300

    feat: add tipo lobo

---
+++ src/types/loboType.tsx
@@ -0,0 +1,13 @@
+interface loboType{
+    id: number
+    nome: string
+    idade: number
+    descricao: string
+    imagem: string
+    adotado: boolean
+    nomeDono?: string
+    idadeDono?: number
+    emailDono?: string
+}
+
+export default loboType
