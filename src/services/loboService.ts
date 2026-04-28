a31db72617a9573b1c1f2c5358c7d2baac9e27b6
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-28 01:12:53 -0300

    feat: add camada de serviços para o projeto

---
+++ src/services/loboService.ts
@@ -0,0 +1,36 @@
+import { HttpAdapter, HttpResponse } from "@/types/http";
+import { baseService } from "./baseService";
+import { apiRoutes } from "@/lib/config";
+import { loboRequest } from "@/types/lobo";
+import loboType from "@/types/loboType";
+
+class LoboService extends baseService {
+    constructor(httpAdapter: HttpAdapter) {
+        super(httpAdapter);
+    }
+
+    public async getLobos(data: loboRequest): Promise<HttpResponse<loboType[]>> {
+        return this.execute<null, loboType[]>({
+            method: "GET",
+            url: apiRoutes.Lobos,
+            params: {
+                _limit: String(data.perPage),
+                _page: String(data.page)
+            }
+        });
+    }
+
+    public async getPijamaById(id: string): Promise<loboType[]> {
+        const response = await this.execute<null, loboType[]>({
+            method: "GET",
+            url: apiRoutes.Lobos,
+            params: {
+                id
+            }
+        });
+
+        return response.data;
+    }
+}
+
+export default LoboService;
