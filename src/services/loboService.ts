d609329303c5346b5962396efc69f547fd9ed71f
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-30 23:38:42 -0300

    feat: add novos métodos para lobo service

--- src/services/loboService.ts
+++ src/services/loboService.ts
@@ -1,12 +1,19 @@
 import { HttpAdapter, HttpResponse } from "@/types/http";
 import { baseService } from "./baseService";
 import { apiRoutes } from "@/lib/config";
-import { loboRequest } from "@/types/lobo";
+import { addPayload, adoptPayload, loboRequest } from "@/types/lobo";
 import loboType from "@/types/loboType";
 
 class LoboService extends baseService {
     constructor(httpAdapter: HttpAdapter) {
         super(httpAdapter);
+    }
+
+    public async getTotalLobos(): Promise<number> {
+        const response = await fetch("http://localhost:3001/lobinhos?_page=1&_per_page=1");
+        const data = await response.json();
+
+        return data.items;
     }
 
     public async getLobos(data: loboRequest): Promise<HttpResponse<loboType[]>> {
@@ -31,6 +38,49 @@ class LoboService extends baseService {
         });
 
         return response.data;
+    }
+
+    public async adoptLobo({ id, nomeDono, idadeDono, emailDono }: adoptPayload): Promise<HttpResponse<loboType>> {
+        return this.execute<Partial<loboType>, loboType>({
+            method: "PATCH",
+            url: `${apiRoutes.Lobos}/${id}`,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: {
+                adotado: true,
+                nomeDono,
+                idadeDono,
+                emailDono
+            }
+        });
+    }
+
+    public async addLobo({ nome, idade, descricao, imagem }: addPayload): Promise<HttpResponse<loboType>> {
+        return this.execute<Partial<loboType>, loboType>({
+            method: "POST",
+            url: apiRoutes.Lobos,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            data: {
+                nome,
+                idade,
+                imagem,
+                descricao,
+                nomeDono: null,
+                emailDono: null,
+                idadeDono: null,
+                adotado: false,
+            }
+        });
+    }
+
+    public async deleteLobo(id: string): Promise<void> {
+        await this.execute<null, loboType>({
+            method: "DELETE",
+            url: `${apiRoutes.Lobos}/${id}`
+        });
     }
 }
 
