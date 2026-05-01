a5342f5a47b27e0654980dac5c8b6874927df866
Pedro Kinupp Rombaldi <kinupprombaldi.pedro@gmail.com>
2026-04-30 23:41:06 -0300

    refactor: att de add-lobinho componentizado

--- src/app/add-lobinho/page.tsx
+++ src/app/add-lobinho/page.tsx
@@ -1,3 +1,4 @@
+import Form from './_components'
 import styles from './styles.module.css'
 
 export default async function AddLobinho( {
@@ -10,27 +11,7 @@ export default async function AddLobinho( {
   return(
     <div className={styles.container}>
       <h1 className={styles.title}>Coloque um Lobinho para Adoção</h1>
-      <form className={styles.form}>
-        <div className={styles.name_age_box}>
-          <div className={styles.name_text_box}>
-            <label className={styles.label}>Nome do Lobinho:</label>
-            <input type="text" className={styles.input} required/>
-          </div>
-          <div className={styles.age_text_box}>
-              <label className={styles.label}>Anos:</label>
-              <input type="number" className={styles.input} required/>
-          </div>
-        </div>
-        <div className={styles.text_box}>
-            <label className={styles.label}>Link da foto:</label>
-            <input type="text" className={styles.input} required/>
-        </div>
-        <div className={styles.text_box}>
-            <label className={styles.label}>Link da foto:</label>
-            <textarea className={styles.input} required/>
-        </div>
-        <button className={styles.button} type="submit">Salvar</button>
-      </form>
+      <Form/>
     </div>
   )
 }
